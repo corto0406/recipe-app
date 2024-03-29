c2ee8980a4da8b68e4be3364b742a2a11d4131e2
corto0406 <132603219+corto0406@users.noreply.github.com>
2024-03-29 11:01:49 +0100

    Configure for Heroku deployment

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -1,19 +1,27 @@
-from pathlib import Path
 import os
-import dj_database_url
+from pathlib import Path
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
-
+# Base directory of your project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Database Configuration
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Secret key
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8hmo67$m5=-x9fwf%n0+_kcn6vx9^epx^^n0_kak$#1!g_u&^z')
 
-DEBUG = os.environ.get('DEBUG', 'False').lower() != 'false'
+# Debug mode
+DEBUG = os.environ.get('DEBUG', 'False').lower() != 'true'
 
-ALLOWED_HOSTS = ['peaceful-shore-26465-668c313a118c.herokuapp.com', '127.0.0.1', 'localhost']
+# Allowed hosts
+ALLOWED_HOSTS = ['gentle-crag-28994.herokuapp.com', '127.0.0.1', 'localhost']
 
+# Installed apps
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -25,6 +33,7 @@ INSTALLED_APPS = [
     'users'
 ]
 
+# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -35,8 +44,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Root URL configuration
 ROOT_URLCONF = 'recipe_project.urls'
 
+# Template configuration
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -53,30 +64,38 @@ TEMPLATES = [
     },
 ]
 
+# WSGI application
 WSGI_APPLICATION = 'recipe_project.wsgi.application'
 
-
-
-
+# Language code
 LANGUAGE_CODE = 'en-us'
 
+# Time zone
 TIME_ZONE = 'UTC'
 
+# Internationalization
 USE_I18N = True
 
+# Localization
 USE_L10N = True
 
+# Time zone support
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'recipe_images')
 MEDIA_URL = '/media/recipe_images/'
 
+# Login redirect URL
 LOGIN_REDIRECT_URL = '/'
 
+# Append slash
 APPEND_SLASH = False
