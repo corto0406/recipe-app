a49f3ee822c23344958fdc2a7c5f5d4a87da875d
corto0406 <132603219+corto0406@users.noreply.github.com>
2024-03-29 15:38:09 +0100

    Initial commit

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8hmo67$m5=-x9fwf%n0+_
 DEBUG = os.environ.get('DEBUG', 'False').lower() != 'true'
 
 # Allowed hosts
-ALLOWED_HOSTS = ['', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['nemanja-food-3b44c7048bc6.herokuapp.com', '127.0.0.1', 'localhost']
 
 # Installed apps
 INSTALLED_APPS = [
