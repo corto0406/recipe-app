bd065ef62da550a03a96a6e3af08fb7127963b98
corto0406 <132603219+corto0406@users.noreply.github.com>
2024-04-07 21:58:23 +0200

    Exercise 2.8

--- recipes/migrations/0002_rename_recipe_recipesrecipe.py
+++ recipes/migrations/0002_rename_recipe_recipesrecipe.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('recipes', '0001_initial'),
+        ('recipes_recipe', '0001_initial'),
     ]
 
     operations = [
