5e6f141ff7d913be474ff66ba03a025fec7ed10d
corto0406 <132603219+corto0406@users.noreply.github.com>
2024-04-07 21:40:04 +0200

    Exercise 2.8

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8hmo67$m5=-x9fwf%n0+_
 
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['recipe-python-2e247ae93449.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
